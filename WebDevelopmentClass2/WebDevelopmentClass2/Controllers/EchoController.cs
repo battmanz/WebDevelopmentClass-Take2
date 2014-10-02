07d949e5543d8170289b3fe36f60417334e53767
Matthew Banz <matt.banz@gmail.com>
2014-10-01 22:24:05 -0600

    Added lesson 3 and echo controller

---
+++ WebDevelopmentClass2/WebDevelopmentClass2/Controllers/EchoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDevelopmentClass2.Controllers
+{
+    public class EchoController : Controller
+    {
+        // GET: Echo
+        public ActionResult Index()
+        {
+            var formCollection = new FormCollection(Request.QueryString);
+            return View(formCollection);
+        }
+
+        // POST: Echo
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            return View(formCollection);
+        }
+    }
+}
